89c799bfb0eb36125c80452d4e951f04b3e981bf
VDE <valentin_2107@yahoo.fr>
2019-07-25 11:09:20 +0100

    jointure

--- src/app/route/choropleth/choropleth.component.ts
+++ src/app/route/choropleth/choropleth.component.ts
@@ -10,6 +10,17 @@ export class ChoroplethComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    this.generateKilledFile();
+  }
+
+  async generateKilledFile() {
+
+    const usagers = await d3.csv('assets/data/usagers-2017.csv');
+    const caracteristique = await d3.csv('assets/data/caracteristiques-2017.csv');
+
+    console.log('usagers', usagers);
+    console.log('caracteristiques', caracteristique);
   }
 
 }
