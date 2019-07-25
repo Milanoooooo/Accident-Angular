7853754f27e654479dd98f13c416a565bb492d89
VDE <valentin_2107@yahoo.fr>
2019-07-25 11:27:57 +0100

    .

--- src/app/route/choropleth/choropleth.component.ts
+++ src/app/route/choropleth/choropleth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-choropleth',
@@ -10,17 +11,32 @@ export class ChoroplethComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-
     this.generateKilledFile();
   }
 
   async generateKilledFile() {
-
     const usagers = await d3.csv('assets/data/usagers-2017.csv');
-    const caracteristique = await d3.csv('assets/data/caracteristiques-2017.csv');
+    const caracteristics = await d3.csv('assets/data/caracteristiques-2017.csv');
 
     console.log('usagers', usagers);
-    console.log('caracteristiques', caracteristique);
+    console.log('caracteristics', caracteristics);
+
+    // filter on killed
+    const killed = usagers.filter(r => r.grav === '2')
+      .map(r => r.Num_Acc);
+    console.log('killed', killed.length);
+
+    //Liste des départements
+    const deps = killed.map(numAcc => {
+      return caracteristics.find(r => r.Num_Acc === numAcc).dep;
+    });
+
+    //Jointure sur la réference de Num_Acc entre les 2 fichiers
+    const result = deps.reduce((acc, n) => {
+      acc[n] = (acc[n] === undefined) ? 1 : acc[n] + 1;
+      return acc;
+    }, {});
+    console.log('result', result);
   }
 
 }
